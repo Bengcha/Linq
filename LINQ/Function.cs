d6b85c0327cd5d4e827eca852d6b20563c47dc3d
Beng Cha <bengcha04@gmail.com>
2017-01-09 08:15:03 -0600

    Adding method and new files for linq assignment

---
+++ LINQ/Function.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Function
+    {
+        public void CarList()
+        {
+            List<string> OldCarList = new List<string>();
+            List<string> NewCarList;
+
+            OldCarList.Add("GT-R");
+            OldCarList.Add("Lamborghini");
+            OldCarList.Add("Corvette");
+            OldCarList.Add("Bugatti");
+            OldCarList.Add("NSX");
+            OldCarList.Add("GT-R");
+            OldCarList.Add("Lamborghini");
+            OldCarList.Add("Corvette");
+            OldCarList.Add("Bugatti");
+            OldCarList.Add("NSX");
+            NewCarList = OldCarList.Distinct().ToList();
+            foreach (string item in NewCarList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void StudentGradeAverage()
+        {
+            List<string> StudentGrade = new List<string>();
+            List<int> GradeAverage = new List<int>(); 
+            StudentGrade.Add("100,90,80,70,60,50");
+            StudentGrade.Add("100,80,60,40,20,0");
+            StudentGrade.Add("100,70,40,10,50,80");
+
+            List<string> Grade = new List<string>();
+            string[] value = StudentGrade.Split(',');
+
+
+
+
+
+        }
+    }
+}
+
+
+
+
+
---
+++ LINQ/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Function newFunction = new Function();
+            newFunction.CarList();
+            newFunction.StudentGradeAverage();
+            Console.ReadKey();
+        }
+    }
+}
